83254e391dee6293e2b358a7edca69d6b4c3998d
DEV-COM <kwp@tlen.pl>
2025-01-08 15:28:02 +0100

    Dodano komponent Alert do App oraz usunięto niepotrzebne elementy

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Alert } from './Alert'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,13 +13,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Alert type='warning' heading='Sukces' closable>
+        test...
+      </Alert>
       </>
   )
 }
