110063bc804e73253cc4b3e052e4b41b411a2d28
DEV-COM <kwp@tlen.pl>
2025-01-09 11:28:50 +0100

    Dodano szkielety komponentów Cykle i Kompo z przykładowymi implementacjami cyklu życia

---
+++ my-app/src/Cykle.tsx
@@ -0,0 +1,14 @@
+// import { useEffect } from "react";
+
+// function Komponent() {
+//     useEffect(() => {
+//     const timer = setInterval(() => {
+//         console.log('timer działa')
+//     }, 1000)
+
+//         return () => {
+//             console.log('czyszczenie')
+//             clearInterval(timer)
+//         }
+//     }, [])
+// }
---
+++ my-app/src/Kompo.tsx
@@ -0,0 +1,12 @@
+// class MyComponent extends React.Component {
+//     componentWillUnmount() {
+//       console.log('Komponent jest usuwany z DOM');
+//       // Czyszczenie zasobów, np.:
+//       clearInterval(this.timer);
+//     }
+  
+//     render() {
+//       return <div>Hello World</div>;
+//     }
+//   }
+  
