cb34d6249ba6c360fb213796cd85b60dcc7ec009
DEV-COM <kwp@tlen.pl>
2025-01-09 09:51:52 +0100

    Zoptymalizowano komponent PersonScore, dodano logikę zarządzania stanem z użyciem useReducer oraz funkcję obliczeniową z memoizacją.

--- my-app/src/PersonScoreNew.tsx
+++ my-app/src/PersonScoreNew.tsx
@@ -1,53 +1,75 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useReducer } from "react";
 import { getPerson } from "./getPerson";
 
 function ExpensiveFunction() {
     console.log('Wykonujemy funkcje obliczeniową')
     let sum = 0
-    for (let i = 0; i < 10000; i++){
+    for (let i = 0; i < 10000; i++) {
         sum += i
     }
     return sum
 }
 
-type State = { 
+type State = {
     name: string | undefined
     score: number
     loading: boolean
 }
-type Action = 
-    |   { 
-    type: 'initialize'
-    name: string
+type Action =
+    | {
+        type: 'initialize'
+        name: string
     }
-    |   {
-    type: 'increment'
+    | {
+        type: 'increment'
     }
-    |   {
-    type: 'decrement'
+    | {
+        type: 'decrement'
     }
-    |   {
-    type: 'reset'   
+    | {
+        type: 'reset'
     }
 
-function reducer(state: State, action: Action):State {
+function reducer(state: State, action: Action): State {
     switch (action.type) {
         case 'initialize':
-            return {name: action.name, score: 0, loading: false}
+            return { name: action.name, score: 0, loading: false }
         case 'increment':
-            return {...state, score: state.score + 1}
+            return { ...state, score: state.score + 1 }
         case 'decrement':
-            return {...state, score: state.score - 1}
+            return { ...state, score: state.score - 1 }
         case 'reset':
-            return {...state, score: 0}
+            return { ...state, score: 0 }
         default:
             return state
     }
 }
 
-export function PersonScore(){
+export function PersonScore() {
+    const [{ name, score, loading }, dispatch] = useReducer(reducer, {
+        name: undefined,
+        score: 0,
+        loading: true,
+    })
+
     useEffect(() => {
-        getPerson().then((person) => console.log(person))
+        getPerson().then(({ name }) => dispatch({ type: 'initialize', name}))
     }, [])
-    return null
+
+    const Calculation = useMemo(() => ExpensiveFunction(),[])
+
+    if (loading) {
+        return <div>Ładowanie komponentu...</div>
+    }
+    return (
+        <>
+        <h3>
+            {name}, {score}
+        </h3>
+        <p>{Calculation}</p>
+        <button onClick={()=> dispatch({ type: 'decrement'})}>-</button>
+        <button onClick={()=> dispatch({ type: 'increment'})}>+</button>
+        <button onClick={()=> dispatch({ type: 'reset'})}>reset</button>
+        </>
+    )
 }
