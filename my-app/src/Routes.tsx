d4e3edb7fc1510c9e04a0ba576b63e6a464dcc4b
DEV-COM <kwp@tlen.pl>
2025-01-09 14:20:40 +0100

    Dodano stronę główną oraz zaktualizowano routing aplikacji

--- my-app/src/Routes.tsx
+++ my-app/src/Routes.tsx
@@ -2,12 +2,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import { ProductsPage } from "./pages/ProductsPage";
 import { ProductPage } from "./pages/ProductPage";
+import { HomePage } from "./pages/HomePage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App/>,
         children:[
+            {
+                index: true,
+                element: <HomePage/>
+            },
             {
                 path: 'products',
                 element: <ProductsPage/>
---
+++ my-app/src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export function HomePage(){
+    return (
+        <div>
+            <h2>Welcome to React</h2>
+        </div>
+    )
+}
